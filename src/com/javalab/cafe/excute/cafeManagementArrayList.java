b0b22c1646ef6e8846e3f5ee995173f4b43267e5
dokim63 <kdo0421772@naver.com>
2024-04-17 23:01:37 +0900

    4월 17일 과제

--- src/com/javalab/cafe/excute/cafeManagementArrayList.java
+++ src/com/javalab/cafe/excute/cafeManagementArrayList.java
@@ -1,5 +1,6 @@
 package com.javalab.cafe.excute;
 
+import com.javalab.cafe.data.ArrayDatabase;
 import com.javalab.cafe.data.ArrayListDatabase;
 import com.javalab.cafe.domain.Category;
 import com.javalab.cafe.domain.Employee;
@@ -30,7 +31,14 @@ public class cafeManagementArrayList {
         showEmployee(employees);
 
         //상품 데이터 출력 - 상품번호,상품명,카테고리id, 카테고리명, 가격
-        showProductsWithCategoryName(categories,products);
+        showProductsWithCategoryName(categories, products);
+
+        //주문 데이터 출력 -  주문 id, 주문일자 , 상품 id, 주문처리사원 id ,주문수량
+
+
+        // 4월 17일 과제 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+        showOrderWithCategoryProduct(orders, employees, products);
+        //
 
     }// end of main
 
@@ -59,41 +67,42 @@ public class cafeManagementArrayList {
 
 
     // 주문 정보 출력 메소드
-    public static void showOrderList(ArrayList<Order> orders){
+    public static void showOrderList(ArrayList<Order> orders) {
         System.out.println("==========================================");
         System.out.println("주문 정보");
         System.out.println("주문ID\t주문날짜\t상품ID\t처리직원ID\t상품수량");
-        for(Order order : orders ){
-            System.out.println(order.getOrderId()+"\t"+order.getOrderDate()+"\t"+
-                    order.getProductId()+"\t"+order.getEmployeeId()+"\t"+order.getQuantity());
+        for (Order order : orders) {
+            System.out.println(order.getOrderId() + "\t" + order.getOrderDate() + "\t" +
+                    order.getProductId() + "\t" + order.getEmployeeId() + "\t" + order.getQuantity());
         }
     } // end of method
 
 
     // 사원 정보 출력 메소드
-    public static void showEmployee(ArrayList<Employee> employees){
+    public static void showEmployee(ArrayList<Employee> employees) {
         System.out.println("==========================================");
         System.out.println("사원 정보");
         System.out.println("ID\t이름\t직급\t연봉");
-        for(Employee employee : employees){
-            System.out.println(employee.getEmployeeId()+"\t"+employee.getEmployeeName()+"\t"
-                    +employee.getEmployeePosition()+"\t"+employee.getEmployeeSalary()
+        for (Employee employee : employees) {
+            System.out.println(employee.getEmployeeId() + "\t" + employee.getEmployeeName() + "\t"
+                    + employee.getEmployeePosition() + "\t" + employee.getEmployeeSalary()
             );
         }
     }
+
     //(상품번호,상품명,카테고리id, 카테고리명, 가격) 출력 메소드
-    public static void showProductsWithCategoryName(ArrayList<Category> categories,ArrayList<Product> products){
+    public static void showProductsWithCategoryName(ArrayList<Category> categories, ArrayList<Product> products) {
         System.out.println("상품ID\t상품명\t카테고리번호\t카테고리명\t상품 가격");
-        for(Product product : products){
-             int categoryId = product.getCategoryId(); //현재 상품의 카테고리ID
+        for (Product product : products) {
+            int categoryId = product.getCategoryId(); //현재 상품의 카테고리ID
             String categoryName = "";
             //for문으로 카테고리 리스트에서 카테고리Id와 일치하는 카테고리 이름 찾기
-                for(Category category : categories){
-                    if(category.getCategoryID() == categoryId){
-                        categoryName = category.getName();
-                        break;
-                    }
+            for (Category category : categories) {
+                if (category.getCategoryID() == categoryId) {
+                    categoryName = category.getName();
+                    break;
                 }
+            }
 
 //                for(Category category : categories){
 //                    if(category.getCategoryID() == product.getCategoryId()){
@@ -101,9 +110,39 @@ public class cafeManagementArrayList {
 //                                category.getName()+"\t"+product.getPrice());
 //                }
             System.out.println(product.getProductid() + "\t" + product.getName() + "\t"
-                    + product.getCategoryId() + "\t"+categoryName +"\t" + product.getPrice());
+                    + product.getCategoryId() + "\t" + categoryName + "\t" + product.getPrice());
         }
     }
+
+    //4월 17일 과제 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+    //주문 id, 주문일자 , 상품 id, 주문처리사원 이름 ,주문수량 메소드
+    public static void showOrderWithCategoryProduct(ArrayList<Order> orders, ArrayList<Employee> employees, ArrayList<Product> products) {
+        for (Order order : orders) {
+            int employeeId = order.getEmployeeId();
+            String employeeName = "";
+            int productId = order.getProductId();
+            String productName = "";
+
+            for(Employee employee : employees) {
+                if(employeeId == employee.getEmployeeId()) {
+                    employeeName = employee.getEmployeeName();
+                    break;
+                }
+            }
+
+
+            for (Product product : products) {
+                if (productId == product.getProductid()) {
+                    productName = product.getName();
+                    break;
+                }
+            }
+
+                    System.out.println(order.getOrderId() + "\t" + order.getOrderDate() + "\t" + productName + "\t" +
+                            employeeName + "\t" + order.getQuantity());
+        }
+
+
+
+    }
 }
-
-
